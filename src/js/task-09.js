922d5acde1c71084be0e6c707404987ead3ce989
deathfluffy <mrandreu454@gmail.com>
2023-10-05 19:08:06 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,11 +3,6 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
 
 const changeColorButton = document.querySelector(".change-color");
 const colorSpan = document.querySelector(".color");
